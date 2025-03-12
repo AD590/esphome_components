2bf673a154029d57c61f7a74ecaea6895a5d9172
AD590 <zad590@gmail.com>
2025-03-12 13:43:00 +0800

    ohhhhhh

--- components/74hc138_platform/hc138_switch.cpp
+++
@@ -1,9 +0,0 @@
-#include "hc138_switch.h"
-
-HC138Switch::HC138Switch(HC138Chip *chip, uint8_t channel)
-    : chip_(chip), channel_(channel) {}
-
-void HC138Switch::write_state(bool state) {
-  chip_->set_output(channel_, state);
-  publish_state(state);
-}
--- components/74hc156_platform/hc156_binary_sensor.cpp
+++
@@ -1,9 +0,0 @@
-#include "hc156_binary_sensor.h"
-
-HC156BinarySensor::HC156BinarySensor(HC156Chip *chip, uint8_t channel, uint32_t update_interval)
-    : PollingComponent(update_interval), chip_(chip), channel_(channel) {}
-
-void HC156BinarySensor::update() {
-  bool state = chip_->read_input(channel_);
-  publish_state(state);
-}
